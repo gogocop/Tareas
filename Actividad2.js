1abbda164fdef847a15f5bd17288d1a54eb8e234
Manuel Rodriguez-Villacorta <22006988@galileo.edu>
2022-02-06 18:23:50 -0600

    Test

---
+++ Actividad2.js
@@ -0,0 +1,28 @@
+var mes = 10
+
+if(mes > 11 && mes < 13){
+  alert("es invierno")
+  alert("22006988")
+  alert("Manuel Enrique Rodriguez-Villacorta")
+
+  }if(mes > 0 && mes < 3){
+  alert("es invierno")
+  alert("22006988")
+  alert("Manuel Enrique Rodriguez-Villacorta")
+  
+  }if(mes > 2 && mes < 6){
+  alert("es primavera")
+  alert("22006988")
+  alert("Manuel Enrique Rodriguez-Villacorta")
+
+  }if(mes > 5 && mes < 9){
+  alert("es verano")
+  alert("22006988")
+  alert("Manuel Enrique Rodriguez-Villacorta")
+  
+  }if(mes > 8 && mes < 12){
+  alert("es otoño")
+  alert("22006988")
+  alert("Manuel Enrique Rodriguez-Villacorta")
+
+  }
